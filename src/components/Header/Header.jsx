4b547cc10b8ce2d9f1c4cfd2a83088502847eeb0
gldsct <188839581+gldsct@users.noreply.github.com>
2026-01-08 21:46:56 +0545

    Style webpage header
    Style the webpage's header, including making it accessible for mobile
    use by implementing dropdown navigation to account for restricted
    viewport widths.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,32 @@
 import cartIcon from "../../assets/images/cart-outline.svg";
 import accountIcon from "../../assets/images/account-box-outline.svg";
 import menuIcon from "../../assets/images/menu.svg";
+import styles from "./Header.module.css";
 
 function Header ({ cartItemsLength }) {
     return (
         <header>
-            <nav aria-label = "main navigation">
-                <div>
+            <nav aria-label = "main navigation" className = {styles["navbar"]}>
+                <div className = {styles["dropdown-menu"]}>
                     <img src = {menuIcon} alt = "Navigate the website" width = {32} height = {32} />
-                    <ul>
+                    <ul className = {styles["shopping-links"]}>
                         <li>Home</li>
                         <li>Shop Now</li>
                         <li>New Arrivals</li>
                     </ul>
                 </div>
 
-                <h1>
+                <h1 className = {styles["navbar-header"]}>
                     ShopCart
                 </h1>
 
-                <ul>
-                    <li>
-                        <p>CART</p>
+                <ul className = {styles["account-links"]}>
+                    <li className = {styles["cart-link"]}>
+                        <p className = {styles["cart-label"]}>CART</p>
                         <img src = {cartIcon} alt = "Access your shopping cart" width = {32} height = {32} />
                         <p>
-                            <span>Total items in cart: </span> 
-                            <span>{cartItemsLength}</span>
+                            <span className = "sr-only">Total items in cart:</span> 
+                            <span className = {styles["cart-quantity"]}>{cartItemsLength}</span>
                         </p>
                     </li>
 
