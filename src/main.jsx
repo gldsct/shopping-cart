2e2e1dad6ab54015b2d1cc5817ef08c4d951dc56
gldsct <188839581+gldsct@users.noreply.github.com>
2026-01-13 21:12:14 +0545

    Create and render browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { HomePage } from "./components/HomePage/HomePage.jsx";
 import App from "./App.jsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      }
+    ],
+  }
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </StrictMode>,
 );
