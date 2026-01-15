939315eed267424f8c143b2168948c5584c99dd3
gldsct <188839581+gldsct@users.noreply.github.com>
2026-01-15 21:14:46 +0545

    Upload mock clothing items data

---
+++ src/clothingItems.js
@@ -0,0 +1,85 @@
+import firstShirtModel from "./assets/Shop/shirt-1.jpg";
+import secondShirtModel from "./assets/Shop/shirt-2.jpg";
+import thirdShirtModel from "./assets/Shop/shirt-3.jpg";
+import fourthShirtModel from "./assets/Shop/shirt-4.jpg";
+import fifthShirtModel from "./assets/Shop/shirt-5.jpg";
+import firstCoatModel from "./assets/Shop/coat-6.jpg";
+import secondCoatModel from "./assets/Shop/coat-7.jpg";
+import thirdCoatModel from "./assets/Shop/coat-8.jpg";
+import fourthCoatModel from "./assets/Shop/coat-9.jpg";
+import fifthCoatModel from "./assets/Shop/coat-10.jpg";
+
+const shoppingItems = [
+  {
+    id: 1,
+    category: "shirt",
+    name: "Casual T-Shirt",
+    price: "37.99",
+    image: firstShirtModel
+  },
+  {
+    id: 2,
+    category: "shirt",
+    name: "Cotton T-Shirt",
+    price: "42.95",
+    image: secondShirtModel
+  },
+  {
+    id: 3,
+    category: "shirt",
+    name: "Denim Shirt",
+    price: "45.00",
+    image: thirdShirtModel
+  },
+  {
+    id: 4,
+    category: "shirt",
+    name: "Striped Shirt",
+    price: "42.99",
+    image: fourthShirtModel
+  },
+  {
+    id: 5,
+    category: "shirt",
+    name: "Plain T-Shirt",
+    price: "25.99",
+    image: fifthShirtModel
+  },
+  {
+    id: 6,
+    category: "coat",
+    name: "Peacoat",
+    price: "599.99",
+    image: firstCoatModel
+  },
+  {
+    id: 7,
+    category: "coat",
+    name: "Trench Coat",
+    price: "148.00",
+    image: secondCoatModel
+  },
+  {
+    id: 8,
+    category: "coat",
+    name: "Single Button Blazer",
+    price: "169.99",
+    image: thirdCoatModel
+  },
+  {
+    id: 9,
+    category: "coat",
+    name: "Relaxed Blazer",
+    price: "74.50",
+    image: fourthCoatModel
+  },
+  {
+    id: 10,
+    category: "coat",
+    name: "Patterned Blazer",
+    price: "150.00",
+    image: fifthCoatModel
+  }
+];
+
+export { shoppingItems };
