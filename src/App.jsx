12e6d06ce3f1928668876e3d083519386d6e4ba4
gldsct <188839581+gldsct@users.noreply.github.com>
2026-01-16 16:10:09 +0545

    Implement full-viewport-height layout for website

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router";
 import { Header } from "./components/Header/Header.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
 import { shoppingItems } from "./clothingItems.js";
+import "./App.css";
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
