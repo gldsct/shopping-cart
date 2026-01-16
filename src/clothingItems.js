9588a33b3a7bc1056ef545f11eb9f1c02e43ed18
gldsct <188839581+gldsct@users.noreply.github.com>
2026-01-16 13:19:40 +0545

    Add alt text for mock data's images

--- src/clothingItems.js
+++ src/clothingItems.js
@@ -15,70 +15,80 @@ const shoppingItems = [
     category: "shirt",
     name: "Casual T-Shirt",
     price: "37.99",
-    image: firstShirtModel
+    image: firstShirtModel,
+    imageAlt: "Man wearing a black half-sleeved t-shirt and blue jeans"
   },
   {
     id: 2,
     category: "shirt",
     name: "Cotton T-Shirt",
     price: "42.95",
-    image: secondShirtModel
+    image: secondShirtModel,
+    imageAlt: "Man wearing a white half-sleeved t-shirt and dark gray pants"
   },
   {
     id: 3,
     category: "shirt",
     name: "Denim Shirt",
     price: "45.00",
-    image: thirdShirtModel
+    image: thirdShirtModel,
+    imageAlt: "Man wearing a denim shirt and khaki pants"
   },
   {
     id: 4,
     category: "shirt",
     name: "Striped Shirt",
     price: "42.99",
-    image: fourthShirtModel
+    image: fourthShirtModel,
+    imageAlt: "Man wearing a blue-and-white striped, full-sleeved shirt"
   },
   {
     id: 5,
     category: "shirt",
     name: "Plain T-Shirt",
     price: "25.99",
-    image: fifthShirtModel
+    image: fifthShirtModel,
+    imageAlt: "Man wearing a plain white half-sleeved t-shirt"
   },
   {
     id: 6,
     category: "coat",
     name: "Peacoat",
     price: "599.99",
-    image: firstCoatModel
+    image: firstCoatModel,
+    imageAlt: "Woman wearing a dark cyan peacoat and light blue jeans, carrying a white handbag"
   },
   {
     id: 7,
     category: "coat",
     name: "Trench Coat",
     price: "148.00",
-    image: secondCoatModel
+    image: secondCoatModel,
+    imageAlt: "Woman wearing a black sweater layered with a black trench coat, clutching a purse"
   },
   {
     id: 8,
     category: "coat",
     name: "Single Button Blazer",
     price: "169.99",
-    image: thirdCoatModel
+    image: thirdCoatModel,
+    imageAlt: "Woman wearing a blue single-button blazer and white pants, holding a yellow handbag"
   },
   {
     id: 9,
     category: "coat",
     name: "Relaxed Blazer",
     price: "74.50",
-    image: fourthCoatModel
+    image: fourthCoatModel,
+    imageAlt: "Woman wearing a purple blazer and dark blue pants, sitting on a wooden bench"
   },
   {
     id: 10,
     category: "coat",
     name: "Patterned Blazer",
     price: "150.00",
-    image: fifthCoatModel
+    image: fifthCoatModel,
+    imageAlt: "Woman wearing a flint-colored turtleneck sweater layered with a grayish-brown-colored patterned blazer, smiling warmly"
   }
 ];
 
