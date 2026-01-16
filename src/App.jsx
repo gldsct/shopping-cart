716e80ab7d920d1cc7359e63fcf0cbd1ce1443a1
gldsct <188839581+gldsct@users.noreply.github.com>
2026-01-16 12:07:22 +0545

    Add cart state and state management functions
    Create a shopping cart state, and functions to modify/manage state.
    Helper functions implement functionality to add to or remove from cart,
    and increase or decrease the quantity of existing items in the cart.

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,39 @@ import { shoppingItems } from "./clothingItems.js";
 function App() {
   const [cartItems, setCartItems] = useState([]);
 
+  function addToCart (itemId, itemQuantity) {
+    if (itemQuantity < 1) return;
+
+    const itemExists = cartItems.some(item => item.id === itemId);
+    if (itemExists) {
+      return setCartItems(cartItems.map(item => {
+        if (item.id !== itemId) return {...item};
+        return {...item, quantity: itemQuantity};
+      }));
+    }
+
+    setCartItems([...cartItems, { id: itemId, quantity: itemQuantity }]);
+  }
+
+  function removeFromCart (itemId) {
+    setCartItems(cartItems.filter(item => item.id !== itemId));
+  }
+
+  function increaseQuantity (itemId) {
+    const currentItem = cartItems.find(item => item.id === itemId);
+    addToCart(itemId, currentItem.quantity + 1);
+  }
+
+  function decreaseQuantity (itemId) {
+    const currentItem = cartItems.find(item => item.id === itemId);
+    if (currentItem.quantity > 1) addToCart(itemId, currentItem.quantity - 1);
+  }
+
   return (
     <>
       <Header cartItemsLength = {cartItems.length} />
       <main>
-        <Outlet />      
+        <Outlet context = { {shoppingItems, cartItems, addToCart, removeFromCart, increaseQuantity, decreaseQuantity} } />      
       </main>
       <Footer />
     </>
